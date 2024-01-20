6a199b1de91d7ce91d90557f7c78dd79296f0908
OliviaWynn <odw23@byu.edu>
2024-01-20 15:20:38 -0700

    implemented basic logic for iterating moves
    passed:
    - ChessPositionTests
    - ChessMoveTests

--- shared/src/main/java/chess/PieceMovement.java
+++ shared/src/main/java/chess/PieceMovement.java
@@ -44,6 +44,26 @@ public class PieceMovement {
         BACKRIGHT
     }
 
+    protected int checkPosition(int maxDistance, int distanceLeft, int i,
+                                ChessBoard board, Collection<ChessMove> moves,
+                                ChessPosition startPos, ChessPosition curPos) {
+        distanceLeft -= 1;
+
+        if (distanceLeft > 0 && board.getPiece(curPos).getPieceType()
+                != board.getPiece(startPos).getPieceType()) {
+
+            //create chessmove and add to collection
+            moves.add(new ChessMove(startPos, curPos));
+
+        }
+        else {
+            i += 1;
+            distanceLeft = maxDistance;
+        }
+
+        return i;
+    }
+
     protected Collection<ChessMove> iterateMoves(ChessBoard board, ChessPosition startPos,
                                                int maxDistance, Direction[] directions) {
         //Directions[] and maxDistance provided by subclass calling this method
@@ -61,46 +81,34 @@ public class PieceMovement {
             switch(Array.get(directions, i)) {
                 case UP:
                     curPos.setRow(curPos.getRow() + 1);
-                    break;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
                 case BACK:
                     curPos.setRow(curPos.getRow() - 1);
-                    break;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
                 case LEFT:
                     curPos.setCol(curPos.getColumn() - 1);
-                    break;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
                 case RIGHT:
                     curPos.setCol(curPos.getColumn() + 1);
-                    break;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
                 case UPLEFT:
                     curPos.setRow(curPos.getRow() + 1);
                     curPos.setCol(curPos.getColumn() - 1);
-                    break;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
                 case UPRIGHT:
                     curPos.setRow(curPos.getRow() + 1);
                     curPos.setCol(curPos.getColumn() + 1);
-                    break;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
                 case BACKLEFT:
                     curPos.setRow(curPos.getRow() - 1);
                     curPos.setCol(curPos.getColumn() - 1);
-                    break;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
                 case BACKRIGHT:
                     curPos.setRow(curPos.getRow() - 1);
                     curPos.setCol(curPos.getColumn() + 1);
-                    break;
-            }
-
-            distanceLeft -= 1;
-
-            if (distanceLeft > 0 && board.getPiece(curPos).getPieceType()
-                    != board.getPiece(startPos).getPieceType()) {
-
-                //create chessmove and add to collection
-                moves.add(new ChessMove(startPos, curPos));
-
-            }
-            else {
-                i = i + 1;
-                distanceLeft = maxDistance;
+                    i = checkPosition(maxDistance, distanceLeft, i, board, moves, startPos, curPos);
+                default:
+                    throw new IllegalStateException("Unexpected value: " + Array.get(directions, i));
             }
 
         }

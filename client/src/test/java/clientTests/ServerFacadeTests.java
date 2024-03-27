a31cf752e04eacd7a739158e7c93b84ac6973a71
OliviaWynn <odw23@byu.edu>
2024-03-26 18:01:20 -0600

    Fixed fascade test directory (again)

--- client/src/test/java/clientTests/ServerFacadeTests.java
+++ client/src/test/java/clientTests/ServerFacadeTests.java
@@ -45,107 +45,107 @@ public class ServerFacadeTests {
     @Test
     void registerSuccess() throws Exception {
         LoginAndRegisterResponse res = fascade.register(new UserData("player2", "password", "p1@email.com"));
-        assertTrue(res.getAuthToken().length() > 10);
+        Assertions.assertTrue(res.getAuthToken().length() > 10);
     }
 
     @Test
     void registerFailAlreadyTaken() throws Exception {
-        Exception ex = assertThrows(Exception.class, () -> fascade.register(new UserData(existsUsername, existsPassword, existsEmail)));
-        assertTrue(ex.getMessage().contains("403"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.register(new UserData(existsUsername, existsPassword, existsEmail)));
+        Assertions.assertTrue(ex.getMessage().contains("403"));
     }
 
     @Test
     void registerFailBadRequest() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.register(new UserData(existsUsername, existsPassword, null)));
-        assertTrue(ex.getMessage().contains("400"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.register(new UserData(existsUsername, existsPassword, null)));
+        Assertions.assertTrue(ex.getMessage().contains("400"));
     }
 
     @Test
     void loginSuccess() throws Exception {
         LoginAndRegisterResponse res = fascade.login(new UserData(existsUsername, existsPassword, null));
-        assertTrue(res.getAuthToken().length() > 10);
+        Assertions.assertTrue(res.getAuthToken().length() > 10);
     }
 
     @Test
     void loginFailUnauthorized() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.login(new UserData(existsUsername, "wrongpassword", null)));
-        assertTrue(ex.getMessage().contains("401"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.login(new UserData(existsUsername, "wrongpassword", null)));
+        Assertions.assertTrue(ex.getMessage().contains("401"));
     }
 
     @Test
     void logoutSuccess() throws Exception {
         LoginAndRegisterResponse res = fascade.logout(existsAuth);
-        assertNull(res.getMessage());
+        Assertions.assertNull(res.getMessage());
     }
 
     @Test
     void logoutFailUnauthorized() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.logout(new AuthData(existsUsername, "bad token")));
-        assertTrue(ex.getMessage().contains("401"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.logout(new AuthData(existsUsername, "bad token")));
+        Assertions.assertTrue(ex.getMessage().contains("401"));
     }
 
     @Test
     void createGameSuccess() throws Exception {
         GameResponse res = fascade.createGame(new GameRequest("NewGame", null, null), existsAuth);
-        assertNotNull(res.getGameID());
-        assertEquals(2, res.getGameID());
+        Assertions.assertNotNull(res.getGameID());
+        Assertions.assertEquals(2, res.getGameID());
     }
 
     @Test
     void createGameFailUnauthorized() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.createGame(new GameRequest("NewGame", null, null),
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.createGame(new GameRequest("NewGame", null, null),
                 new AuthData(existsUsername, "bad token")));
-        assertTrue(ex.getMessage().contains("401"));
+        Assertions.assertTrue(ex.getMessage().contains("401"));
     }
 
     @Test
     void createGameBadRequest() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.createGame(new GameRequest(null, null, null), existsAuth));
-        assertTrue(ex.getMessage().contains("400"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.createGame(new GameRequest(null, null, null), existsAuth));
+        Assertions.assertTrue(ex.getMessage().contains("400"));
     }
 
     @Test
     void joinGameSuccess() throws Exception {
         GameResponse res = fascade.joinGame(new GameRequest(null, 1, "white"), existsAuth);
-        assertNull(res.getMessage());
+        Assertions.assertNull(res.getMessage());
     }
 
     @Test
     void joinGameFailUnauthorized() throws Exception {
-        Exception ex = assertThrows(Exception.class, () -> fascade.joinGame(new GameRequest(null, 1, null),
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.joinGame(new GameRequest(null, 1, null),
                         new AuthData(existsUsername, "bad token")));
-        assertTrue(ex.getMessage().contains("401"));
+        Assertions.assertTrue(ex.getMessage().contains("401"));
     }
 
     @Test
     void joinGameFailBadRequestNoID() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.joinGame(new GameRequest(null, null, null), existsAuth));
-        assertTrue(ex.getMessage().contains("400"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.joinGame(new GameRequest(null, null, null), existsAuth));
+        Assertions.assertTrue(ex.getMessage().contains("400"));
     }
 
     @Test
     void joinGameFailIDNotExists() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.joinGame(new GameRequest(null, 100, null), existsAuth));
-        assertTrue(ex.getMessage().contains("400"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.joinGame(new GameRequest(null, 100, null), existsAuth));
+        Assertions.assertTrue(ex.getMessage().contains("400"));
     }
 
     @Test
     void joinGameFailSpotTaken() throws Exception {
         fascade.joinGame(new GameRequest(null, 1, "white"), existsAuth);
-        Exception ex = assertThrows(Exception.class, () -> fascade.joinGame(
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.joinGame(
                 new GameRequest(null, 1, "white"), existsAuth));
-        assertTrue(ex.getMessage().contains("403"));
+        Assertions.assertTrue(ex.getMessage().contains("403"));
     }
 
     @Test
     void listGamesSuccess() throws Exception {
         GameListResponse res = fascade.listGames(existsAuth);
-        assertTrue(!res.getGameList().isEmpty());
+        Assertions.assertTrue(!res.getGameList().isEmpty());
     }
 
     @Test
     void ListGamesFailUnauthorized() {
-        Exception ex = assertThrows(Exception.class, () -> fascade.listGames(new AuthData(existsUsername, "bad token")));
-        assertTrue(ex.getMessage().contains("401"));
+        Exception ex = Assertions.assertThrows(Exception.class, () -> fascade.listGames(new AuthData(existsUsername, "bad token")));
+        Assertions.assertTrue(ex.getMessage().contains("401"));
     }
 }
